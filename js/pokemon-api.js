bf84ea4cb382b259e441c23704aa7b44f858e8da
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-02-24 21:18:39 +0200

    update

--- js/pokemon-api.js
+++ js/pokemon-api.js
@@ -1,14 +1,26 @@
 // import pokemonCardTpl  from '../templates/pokemon-card.hbs'
 
 fetch('https://pokeapi.co/api/v2/pokemon/2')
-    .then(response => {
-        return response.json();
-    })
-    .then(pokemon => {
-        console.log(pokemon);
-        // const markup = pokemonCardTpl(pokemon);
-        // console.log(markup);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+  .then(response => {
+    return response.json();
+  })
+  .then(pokemon => {
+    console.log(pokemon);
+
+    // const markup = pokemonCardTpl(pokemon);
+    // console.log(markup);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+function createMarkup(keysPokemon) {
+  return keysPokemon
+    .map(
+      (name, weight, height) => `<li>
+        <p>${'name'}</p>
+        <h2>${'weight'}</h2>
+        <h3>${'height'}</h3>
+      </li>`,
+    )
+    .join('');
+}
